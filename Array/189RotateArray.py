797077d2ababc7cf9f0ce1606e3327f7a6db4852
GavinYin <605661532@qq.com>
2016-08-02 21:15:48 +0800

    add 88

--- Array/189RotateArray.py
+++ Array/189RotateArray.py
@@ -15,3 +15,20 @@ class Solution(object):
         for m in range(l):
         	nums[m] = s[m]
         return 
+
+############################################第二种方法
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        l = []
+        l = nums[0:k-1]
+        l.reverse()
+        nums = nums[k:l-1]
+        nums.reverse()
+        nums = l + nums
+        nums.reverse()
+        return
